91c946036f93e6af151800f59ffff8f1f59c506f
ginberg <ginberg@gmail.com>
2018-02-09 12:46:15 +0800

    add shiny apps after start and UI stage

---
+++ ui/app.R
@@ -0,0 +1,18 @@
+library(shiny)
+crypto <- read.csv("crypto-data.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(
+  titlePanel('Cryptocurrency dashboard'),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('selectCoins', 'Select crypto(s)', choices = c("BTC", "ETH", "LTC"), multiple = T),
+      selectInput('selectOutput', 'Select output', choices = c('Market Cap' = "market", Price = "price")),
+      dateRangeInput('selectDate', 'Select date', start = min(crypto$Date), end = max(crypto$Date))
+    ),
+    mainPanel("TODO")
+  )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui = ui, server = server)
