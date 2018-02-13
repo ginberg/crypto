46da6394a341dd445779b2dcf96f194a96f01e06
ginberg <ginberg@gmail.com>
2018-02-13 15:56:30 +0800

    update test script

--- run_tests.R
+++ run_tests.R
@@ -1,4 +1,9 @@
+library(testthat)
 library(shinytest)
 
-# execute shiny tests
-testApp("final")
+test_that("Application works", {
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+  expect_pass(testApp("final", compareImages = FALSE))
+})
